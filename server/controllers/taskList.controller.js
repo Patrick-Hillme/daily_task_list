f976a98e12624ad1a6a7f7334c2bf6cabfd5575f
Patrick-Hillme <path1727@gmail.com>
2023-09-05 21:16:27 -0500

    Added all CRUD functionality to task list DB.

--- server/controllers/taskList.controller.js
+++ server/controllers/taskList.controller.js
@@ -1,6 +1,8 @@
 const TaskList = require('../models/taskList.model')
 
 //! Task List
+
+//* Create a new Task
 module.exports.createNewTask = (req, res) => {
     TaskList.create(req.body)
     .then(newTask => {
@@ -9,5 +11,53 @@ module.exports.createNewTask = (req, res) => {
     .catch((err) => {
         console.log(err)
         res.status(500).json({ message: 'Internal Server Error' })
+    });
+};
+
+// Read all tasks
+module.exports.getAllTasks = (req, res) => {
+    TaskList.find({})
+    .then((allTasks) => {
+        res.json(allTasks)
     })
-}
+    .catch((err) => {
+        res.status(400).json(err)
+    });
+};
+
+//* Read One Task
+module.exports.getOneTask = (req,res) => {
+    TaskList.findOne({ _id: req.params.id })
+    .then(oneTask => {
+        res.json(oneTask)
+    })
+    .catch((err) => {
+        res.status(400).json(err)
+    });
+};
+
+//* Update one Task
+module.exports.updateOneTask = (req,res) => {
+    TaskList.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        {new: true, runValidators: true}
+    )
+    .then(updateTask => {
+        res.json(updateTask)
+    })
+    .catch((err) => {
+        res.status(400).json(err)
+    });
+};
+
+//* Delete Existing Task
+module.exports.deleteExistingTask = (req, res) => {
+    TaskList.deleteOne({ _id: req.params.id })
+        .then(deleteTask => {
+            res.json(deleteTask)
+        })
+        .catch((err) => {
+            res.status(400).json(err)
+        });
+};
--- server/routes/taskList.routes.js
+++ server/routes/taskList.routes.js
@@ -3,4 +3,8 @@ const TaskListController = require('../controllers/taskList.controller')
 //! Task List Routes
 module.exports = app => {
     app.post('/api/newTask', TaskListController.createNewTask)
+    app.get('/api/allTasks', TaskListController.getAllTasks)
+    app.get('/api/oneTask/:id', TaskListController.getOneTask)
+    app.patch('/api/updateTask/:id', TaskListController.updateOneTask)
+    app.delete('/api/deleteTask/:id', TaskListController.deleteExistingTask)
 }
